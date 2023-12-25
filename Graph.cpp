10c02560ff6ed4a2e87ad4464a0c6e68b8d96549
AtesBurak1 <burakates5511@gmail.com>
2023-12-25 16:56:18 +0300

    bug fixes, and line alignments

--- Graph.cpp
+++ Graph.cpp
@@ -5,13 +5,16 @@ Graph::Graph(std::string txt_name, std::string distance_name)
   std::ifstream infile(txt_name);
   std::string line;
 
-  while (std::getline(infile, line)) {
+  while (std::getline(infile, line))
+  {
     std::vector<std::string> vertices;
     size_t pos = 0;
     std::string token;
     std::string initialCity;
     bool initalCityFlag = true;
-    while ((pos = line.find(',')) != std::string::npos) {
+    
+    while ((pos = line.find(',')) != std::string::npos)
+    {
       token = line.substr(0, pos);
       if(initalCityFlag)
       {
@@ -26,7 +29,8 @@ Graph::Graph(std::string txt_name, std::string distance_name)
     }
 
     vertices.push_back(line); // Adding the last vertex without erasing
-    if (!vertices.empty()) {
+    if (!vertices.empty()) 
+    {
       this->adj[initialCity] = vertices;
     }
   }
@@ -34,18 +38,20 @@ Graph::Graph(std::string txt_name, std::string distance_name)
   std::ifstream infile_distance(distance_name);
   std::string line_distance;
   int row = 0;
-  while (std::getline(infile_distance, line_distance)) {
+  while (std::getline(infile_distance, line_distance)) 
+  {
     std::istringstream iss(line_distance);
     std::string value;
     int col = 0;
     int skips = 0;
-    while (std::getline(iss, value, ';')) {
+    while (std::getline(iss, value, ';')) 
+    {
       if (skips < 2) {
-          // Skip the first 2
-          skips++;
-          continue;
+        // Skip the first 2
+        skips++;
+        continue;
       }
-     // Convert the string distance value to integer
+      // Convert the string distance value to integer
       this->distance_matrix[row][col] = std::stoi(value);
       ++col;
     }
@@ -60,8 +66,6 @@ std::vector <std::string> Graph::neighbors(std::string vertex)
   return this->adj[vertex];
 }
 
-
-
 int Graph::get_edge_value(std::string vertex_src, std::string vertex_dst)
 {
   
@@ -75,6 +79,7 @@ int Graph::get_edge_value(std::string vertex_src, std::string vertex_dst)
 void Graph::set_edge_value(std::string vertex_src, std::string vertex_dst, int weight)
 {
   auto it = find(this->adj[vertex_src].begin(), this->adj[vertex_src].end(), vertex_dst);
+
   if(it != adj[vertex_src].end()) // if an edge is found between cities
   {
     this->distance_matrix[ getCityEnum(vertex_src) ][ getCityEnum(vertex_dst) ] = weight;
@@ -83,17 +88,16 @@ void Graph::set_edge_value(std::string vertex_src, std::string vertex_dst, int w
     std::cout << "Successfullys rearranged! "<< vertex_src << "<-->" << vertex_dst << "= " << weight << std::endl;
 
   }
-  else{
+  else
+  {
     std::cout << "There is no direct edge between these two cities!" << std::endl;
   }
 }
-
 
 int Graph::verticies()
 {
   return CITIES; 
 }
-
 
 int Graph::edges()
 {
@@ -106,7 +110,8 @@ int Graph::edges()
   sum = sum / 2;
   return sum;
 }
-// Fot debugging
+
+// For debugging
 void print_pq(    std::priority_queue<std::pair<int, std::string>, std::vector<std::pair<int, std::string>>, std::greater<std::pair<int, std::string>>> pq)
 {
   while (!pq.empty()) { // print current queue for debug
@@ -118,6 +123,7 @@ void print_pq(    std::priority_queue<std::pair<int, std::string>, std::vector<s
   std::cout << std::endl;
 
 }
+
 void Graph::print_path(std::vector <std::string> &path)
 {
   if(path.size() > 0)
@@ -137,6 +143,7 @@ void Graph::print_path(std::vector <std::string> &path)
     std::cout << "No path is found check Start/End city name it should start with camel case!" << std::endl;
   }
 }
+
 void Graph::UFS(std::string src, std::string dst)
 {
   int cost {0};
@@ -146,55 +153,64 @@ void Graph::UFS(std::string src, std::string dst)
   std::cout << "Total Cost: " << cost << std::endl; 
 
 }
+
 std::vector< std::string> Graph::uniform_cost_search(std::string src, std::string dst, int &cost, int *visited) {
-    std::map<std::string, int> dist; // To store distances
-    std::map<std::string, std::string> prev; // To store previous node in the shortest path
-    // Priority queue for (distance, city) query (low distance will be in front of the queue)
-    std::priority_queue<std::pair<int, std::string>, std::vector<std::pair<int, std::string>>, std::greater<std::pair<int, std::string>>> pq;
 
-    //Set initial distance to max (for fnid shortest one)
-    for (const auto& node : this->adj) {
-        dist[node.first] = 9999999;
+  std::map<std::string, int> dist; // To store distances
+  std::map<std::string, std::string> prev; // To store previous node in the shortest path
+  // Priority queue for (distance, city) query (low distance will be in front of the queue)
+  std::priority_queue<std::pair<int, std::string>, std::vector<std::pair<int, std::string>>, std::greater<std::pair<int, std::string>>> pq;
+
+  //Set initial distance to max (for find shortest one)
+  for (const auto& node : this->adj)
+  {
+    dist[node.first] = 9999999;
+  }
+  dist[src] = 0;
+  pq.push({0, src}); // inital state
+  while (!pq.empty()) 
+  {   
+    // loop until queue is empty
+    std::string currentCity;
+    int currentDistance = pq.top().first; // current distance
+    currentCity = pq.top().second; // current city
+    
+    pq.pop(); // visited status
+    visited[getCityEnum(currentCity)] = 1; // mark as visited
+
+    if (currentCity == dst) 
+    { // shortest path found !
+      std::vector<std::string> path;
+      std::string current = dst;
+      while (current != src) 
+      {
+        path.push_back(current);
+        current = prev[current];
+      }
+      path.push_back(src);
+      reverse(path.begin(), path.end()); // reverse the path because of previous list
+      cost = dist[currentCity]; // total cost
+      return path;
     }
-    dist[src] = 0;
-    pq.push({0, src}); // inital state
 
-    while (!pq.empty()) { // loop until queue is empty
-        std::string currentCity;
-        int currentDistance = pq.top().first; // current distance
-        currentCity = pq.top().second; // current city
-        
-        pq.pop(); // visited status
-        visited[getCityEnum(currentCity)] = 1; // mark as visited
-
-        if (currentCity == dst) { // shortest path found !
-            std::vector<std::string> path;
-            std::string current = dst;
-            while (current != src) {
-                path.push_back(current);
-                current = prev[current];
-            }
-            path.push_back(src);
-            reverse(path.begin(), path.end()); // reverse the path because of previous list
-            cost = dist[currentCity]; // total cost
-            return path;
-        }
-
-        if (currentDistance > dist[currentCity]) {
-            continue; // already founded shorter path to this city
-        }
-
-        for (const auto& neighbor : adj[currentCity]) { // visit every neighbor of current city
-            int weight = this->distance_matrix[ getCityEnum(currentCity) ][ getCityEnum(neighbor) ] + dist[currentCity];
-            if (weight < dist[neighbor]) { 
-                  //If current cost is lower than before update the prev list, and distance
-                  prev[neighbor] = currentCity;
-                  dist[neighbor] = weight;
-                  pq.push({weight, neighbor});
-            }
-        }
+    if (currentDistance > dist[currentCity]) 
+    {
+      continue; // already founded shorter path to this city
     }
-    return {}; // path is not find 
+
+    for (const auto& neighbor : adj[currentCity]) 
+    { // visit every neighbor of current city
+      int weight = this->distance_matrix[ getCityEnum(currentCity) ][ getCityEnum(neighbor) ] + dist[currentCity];
+      if (weight < dist[neighbor])
+      { 
+        //If current cost is lower than before update the prev list, and distance
+        prev[neighbor] = currentCity;
+        dist[neighbor] = weight;
+        pq.push({weight, neighbor});
+      }
+    }
+  }
+  return {}; // path is not find 
 }
 
 void Graph::k_nearest(std::string src, int k)
@@ -211,39 +227,47 @@ std::vector <std::string> Graph::k_nearest_search(std::string src, int *visited,
   std::vector <std::string> visited_cities;
   // Priority queue for (distance, city) query (low distance will be in front of the queue)
   std::priority_queue<std::pair<int, std::string>, std::vector<std::pair<int, std::string>>, std::greater<std::pair<int, std::string>>> pq;
-  //Set initial distance to max (for fnid shortest one)
-  for (const auto& node : this->adj) {
+  //Set initial distance to max 
+  for (const auto& node : this->adj)
+  {
       dist[node.first] = 9999999;
   }
   dist[src] = 0;
   pq.push({0, src}); // inital state
-  while (!pq.empty()) { // loop until queue is empty
-      //print_pq(pq); debug
-      std::string currentCity;
-      int currentDistance = pq.top().first; // current distance
-      currentCity = pq.top().second; // current city
-      
-      pq.pop(); // visited status
-      visited[getCityEnum(currentCity)] = 1; // mark as visited
-      if(currentCity != src && find(visited_cities.begin(),visited_cities.end(), currentCity) == visited_cities.end())
-      {
-        visited_cities.push_back(currentCity);
-        count++; // count the cities when we pop!
+  while (!pq.empty()) 
+  {   // loop until queue is empty
+    //print_pq(pq); debug
+    std::string currentCity;
+    int currentDistance = pq.top().first; // current distance
+    currentCity = pq.top().second; // current city
+    
+    pq.pop(); 
+    if (currentDistance > dist[currentCity])
+    {
+      continue; // already founded shorter path to this city
+    }
+    visited[getCityEnum(currentCity)] = 1; // mark as visited
+
+    if(currentCity != src && find(visited_cities.begin(),visited_cities.end(), currentCity) == visited_cities.end())
+    {
+      visited_cities.push_back(currentCity);
+      count++; // count the cities when we pop!
+    }
+
+    if (count == k)
+    { // shortest path found !
+      return visited_cities;
+    }
+
+    for (const auto& neighbor : adj[currentCity]) 
+    { // visit every neighbor of current city
+      int weight = this->distance_matrix[ getCityEnum(currentCity) ][ getCityEnum(neighbor) ] + dist[currentCity];
+      if (weight < dist[neighbor])
+      { 
+            dist[neighbor] = weight;
+            pq.push({weight, neighbor});
       }
-      if (count == k) { // shortest path found !
-          return visited_cities;
-      }
-      if (currentDistance > dist[currentCity]) {
-          continue; // already founded shorter path to this city
-      }
-      for (const auto& neighbor : adj[currentCity]) { // visit every neighbor of current city
-          int weight = this->distance_matrix[ getCityEnum(currentCity) ][ getCityEnum(neighbor) ] + dist[currentCity];
-          if (weight < dist[neighbor]) { 
-                //If current cost is lower than before update the prev list, and distance
-                dist[neighbor] = weight;
-                pq.push({weight, neighbor});
-          }
-      }
+    }
   }
   return {}; // no cities 
 
